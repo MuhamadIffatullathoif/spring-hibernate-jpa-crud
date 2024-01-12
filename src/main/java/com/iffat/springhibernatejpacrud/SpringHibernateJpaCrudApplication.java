48c5b24136dede045f9c0e43bf8bfa8d22696124
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-12 21:40:36 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springhibernatejpacrud/SpringHibernateJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springhibernatejpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateJpaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateJpaCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iffat/springhibernatejpacrud/SpringHibernateJpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springhibernatejpacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateJpaCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

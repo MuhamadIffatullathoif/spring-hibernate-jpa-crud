42fac05c5a063785af52c80c9543df8ff9284477
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-13 06:40:16 +0700

    07. Query Objects with JPA

--- src/main/java/com/iffat/springhibernatejpacrud/SpringHibernateJpaCrudApplication.java
+++ src/main/java/com/iffat/springhibernatejpacrud/SpringHibernateJpaCrudApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringHibernateJpaCrudApplication {
 
@@ -19,8 +21,19 @@ public class SpringHibernateJpaCrudApplication {
         return runner -> {
             // createStudent(studentDAO);
             // createMultipleStudent(studentDAO);
-            readStudent(studentDAO);
+            // readStudent(studentDAO);
+            queryForStudents(studentDAO);
         };
+    }
+
+    private void queryForStudents(StudentDAO studentDAO) {
+        // get a list of students
+        List<Student> theStudents = studentDAO.findAll();
+
+        // display list of students
+        for (Student student: theStudents) {
+            System.out.println(student);
+        }
     }
 
     private void readStudent(StudentDAO studentDAO) {
--- src/main/java/com/iffat/springhibernatejpacrud/dao/StudentDAO.java
+++ src/main/java/com/iffat/springhibernatejpacrud/dao/StudentDAO.java
@@ -2,8 +2,11 @@ package com.iffat.springhibernatejpacrud.dao;
 
 import com.iffat.springhibernatejpacrud.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student studentDao);
     Student findById(Integer id);
+    List<Student> findAll();
 }
